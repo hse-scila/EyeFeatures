12d8ae59b6489a01b1ca2764b25c6701e343a866
vagiz.daudov <vagiz.0282@gmail.com>
2024-03-19 14:49:24 +0300

    Adding saccade acceleration

--- eyetracking/features/stats.py
+++ eyetracking/features/stats.py
@@ -67,6 +67,165 @@ class SaccadeLength(BaseTransformer):
         return features_df if self.return_df else features_df.values
 
 
+class SaccadeAcceleration(BaseTransformer):
+    """
+    Acceleration: dx = v0 * t + 1/2 * a * t^2.
+    Above formula is law of uniformly accelerated
+    motion (TODO consider another way to calculate acceleration).
+    """
+    def __init__(
+            self,
+            stats: List[str],
+            x: str = None,
+            y: str = None,
+            t: str = None,
+            duration: str = None,
+            dispersion: str = None,
+            aoi: str = None,
+            pk: List[str] = None,
+            return_df: bool = True,
+            eps: float = 1e-8,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            t=t,
+            duration=duration,
+            dispersion=dispersion,
+            aoi=aoi,
+            pk=pk,
+            return_df=return_df,
+        )
+        self.stats = stats
+        self.eps = eps
+
+    def check_init(self) -> None:
+        assert self.x is not None, "Error: provide x column before calling transform"
+        assert self.y is not None, "Error: provide y column before calling transform"
+        assert self.t is not None, "Error: provide t column before calling transform"
+
+    @jit(forceobj=True, looplift=True)
+    def transform(self, X: pd.DataFrame) -> Union[pd.DataFrame, np.ndarray]:
+        if self.stats is None:
+            return X if self.return_df else X.values
+
+        self.check_init()
+
+        if self.pk is None:
+            dx = X[self.x].diff()
+            dy = X[self.y].diff()
+            dr = np.sqrt(dx ** 2 + dy ** 2)
+            if self.duration is None:
+                dur = X[self.t].diff().shift(-1).fillna(0)
+                dt = X[self.t] - (X[self.t] + dur / 1000).shift(1)
+            else:
+                dt = X[self.t] - (X[self.t] + X[self.duration] / 1000).shift(1)
+            sac_acc: pd.DataFrame = dr / (dt ** 2 + self.eps) * 1 / 2
+            feature_names = [f"sac_acc_{stat}" for stat in self.stats]
+            gathered_features = [sac_acc.apply(stat) for stat in self.stats]
+        else:
+            groups = X[self.pk].drop_duplicates().values
+            feature_names = []
+            gathered_features = []
+            for group in groups:
+                current_X = X[pd.DataFrame(X[self.pk] == group).all(axis=1)]
+                dx = current_X[self.x].diff()
+                dy = current_X[self.y].diff()
+                dr = np.sqrt(dx ** 2 + dy ** 2)
+                if self.duration is None:
+                    dur = current_X[self.t].diff().shift(-1).fillna(0)
+                    dt = current_X[self.t] - (current_X[self.t] + dur / 1000).shift(1)
+                else:
+                    dt = current_X[self.t] - (
+                        current_X[self.t] + current_X[self.duration] / 1000
+                    ).shift(1)
+                sac_acc: pd.DataFrame = dr / (dt ** 2 + self.eps) * 1 / 2
+                for stat in self.stats:
+                    feature_names.append(
+                        f'sac_acc_{stat}_{"_".join([str(g) for g in group])}'
+                    )
+                    gathered_features.append(sac_acc.apply(stat))
+
+        features_df = pd.DataFrame(data=[gathered_features], columns=feature_names)
+
+        return features_df if self.return_df else features_df.values
+
+class SaccadeVelocity(BaseTransformer):
+    def __init__(
+        self,
+        stats: List[str],
+        x: str = None,
+        y: str = None,
+        t: str = None,
+        duration: str = None,
+        dispersion: str = None,
+        aoi: str = None,
+        pk: List[str] = None,
+        return_df: bool = True,
+        eps: float = 1e-8,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            t=t,
+            duration=duration,
+            dispersion=dispersion,
+            aoi=aoi,
+            pk=pk,
+            return_df=return_df,
+        )
+        self.stats = stats
+        self.eps = eps
+
+    @jit(forceobj=True, looplift=True)
+    def transform(self, X: pd.DataFrame) -> Union[pd.DataFrame, np.ndarray]:
+        if self.stats is None:
+            return X if self.return_df else X.values
+
+        assert self.x is not None, "Error: provide x column before calling transform"
+        assert self.y is not None, "Error: provide y column before calling transform"
+        assert self.t is not None, "Error: provide t column before calling transform"
+
+        if self.pk is None:
+            dx = X[self.x].diff()
+            dy = X[self.y].diff()
+            dr = np.sqrt(dx ** 2 + dy ** 2)
+            if self.duration is None:
+                dur = X[self.t].diff().shift(-1).fillna(0)
+                dt = X[self.t] - (X[self.t] + dur / 1000).shift(1)  # TODO consider units for t/duration
+            else:
+                dt = X[self.t] - (X[self.t] + X[self.duration] / 1000).shift(1)
+            sac_vel: pd.DataFrame = dr / (dt + self.eps)
+            column_names = [f"sac_vel_{stat}" for stat in self.stats]
+            gathered_features = [sac_vel.apply(stat) for stat in self.stats]
+        else:
+            groups = X[self.pk].drop_duplicates().values
+            column_names = []
+            gathered_features = []
+            for group in groups:
+                current_X = X[pd.DataFrame(X[self.pk] == group).all(axis=1)]
+                dx = current_X[self.x].diff()
+                dy = current_X[self.y].diff()
+                dr = np.sqrt(dx ** 2 + dy ** 2)
+                if self.duration is None:
+                    dur = current_X[self.t].diff().shift(-1).fillna(0)
+                    dt = current_X[self.t] - (current_X[self.t] + dur / 1000).shift(1)
+                else:
+                    dt = current_X[self.t] - (
+                        current_X[self.t] + current_X[self.duration] / 1000
+                    ).shift(1)
+                sac_vel: pd.DataFrame = dr / (dt + self.eps)
+                for stat in self.stats:
+                    column_names.append(
+                        f'sac_vel_{stat}_{"_".join([str(g) for g in group])}'
+                    )
+                    gathered_features.append(sac_vel.apply(stat))
+
+        features_df = pd.DataFrame(data=[gathered_features], columns=column_names)
+
+        return features_df if self.return_df else features_df.values
+
+
 class FixationDuration(BaseTransformer):
     def __init__(
         self,
@@ -127,7 +286,7 @@ class FixationDuration(BaseTransformer):
         return features_df if self.return_df else features_df.values
 
 
-class SaccadeVelocity(BaseTransformer):
+class FixationVAD(BaseTransformer):
     def __init__(
         self,
         stats: List[str],
@@ -139,7 +298,6 @@ class SaccadeVelocity(BaseTransformer):
         aoi: str = None,
         pk: List[str] = None,
         return_df: bool = True,
-        eps: float = 1e-8,
     ):
         super().__init__(
             x=x,
@@ -152,51 +310,32 @@ class SaccadeVelocity(BaseTransformer):
             return_df=return_df,
         )
         self.stats = stats
-        self.eps = eps
 
     @jit(forceobj=True, looplift=True)
     def transform(self, X: pd.DataFrame) -> Union[pd.DataFrame, np.ndarray]:
         if self.stats is None:
             return X if self.return_df else X.values
 
-        assert self.x is not None, "Error: provide x column before calling transform"
-        assert self.y is not None, "Error: provide y column before calling transform"
-        assert self.t is not None, "Error: provide t column before calling transform"
+        assert (
+            self.dispersion is not None
+        ), "Error: provide dispersion column before calling transform"
 
         if self.pk is None:
-            dx = X[self.x].diff()
-            dy = X[self.y].diff()
-            dr = np.sqrt(dx ** 2 + dy ** 2)
-            if self.duration is None:
-                dur = X[self.t].diff().shift(-1).fillna(0)
-                dt = X[self.t] - (X[self.t] + dur / 1000).shift(1)
-            else:
-                dt = X[self.t] - (X[self.t] + X[self.duration] / 1000).shift(1)
-            sac_vel: pd.DataFrame = dr / (dt + self.eps)
-            column_names = [f"sac_vel_{stat}" for stat in self.stats]
-            gathered_features = [sac_vel.apply(stat) for stat in self.stats]
+            fix_vad: pd.DataFrame = X[self.dispersion]
+            column_names = [f"fix_disp_{stat}" for stat in self.stats]
+            gathered_features = [fix_vad.apply(stat) for stat in self.stats]
         else:
             groups = X[self.pk].drop_duplicates().values
             column_names = []
             gathered_features = []
             for group in groups:
                 current_X = X[pd.DataFrame(X[self.pk] == group).all(axis=1)]
-                dx = current_X[self.x].diff()
-                dy = current_X[self.y].diff()
-                dr = np.sqrt(dx ** 2 + dy ** 2)
-                if self.duration is None:
-                    dur = current_X[self.t].diff().shift(-1).fillna(0)
-                    dt = current_X[self.t] - (current_X[self.t] + dur / 1000).shift(1)
-                else:
-                    dt = current_X[self.t] - (
-                        current_X[self.t] + current_X[self.duration] / 1000
-                    ).shift(1)
-                sac_vel: pd.DataFrame = dr / (dt + self.eps)
+                fix_vad: pd.DataFrame = current_X[self.dispersion]
                 for stat in self.stats:
                     column_names.append(
-                        f'sac_vel_{stat}_{"_".join([str(g) for g in group])}'
+                        f'fix_disp_{stat}_{"_".join([str(g) for g in group])}'
                     )
-                    gathered_features.append(sac_vel.apply(stat))
+                    gathered_features.append(fix_vad.apply(stat))
 
         features_df = pd.DataFrame(data=[gathered_features], columns=column_names)
 
@@ -256,60 +395,4 @@ class RegressionCount(BaseTransformer):
                 column_names.append(f'reg_count_{"_".join([str(g) for g in group])}')
                 gathered_features.append(reg_count)
         features_df = pd.DataFrame(data=[gathered_features], columns=column_names)
-        return features_df if self.return_df else features_df.values
-
-
-class FixationVAD(BaseTransformer):
-    def __init__(
-        self,
-        stats: List[str],
-        x: str = None,
-        y: str = None,
-        t: str = None,
-        duration: str = None,
-        dispersion: str = None,
-        aoi: str = None,
-        pk: List[str] = None,
-        return_df: bool = True,
-    ):
-        super().__init__(
-            x=x,
-            y=y,
-            t=t,
-            duration=duration,
-            dispersion=dispersion,
-            aoi=aoi,
-            pk=pk,
-            return_df=return_df,
-        )
-        self.stats = stats
-
-    @jit(forceobj=True, looplift=True)
-    def transform(self, X: pd.DataFrame) -> Union[pd.DataFrame, np.ndarray]:
-        if self.stats is None:
-            return X if self.return_df else X.values
-
-        assert (
-            self.dispersion is not None
-        ), "Error: provide dispersion column before calling transform"
-
-        if self.pk is None:
-            fix_vad: pd.DataFrame = X[self.dispersion]
-            column_names = [f"fix_disp_{stat}" for stat in self.stats]
-            gathered_features = [fix_vad.apply(stat) for stat in self.stats]
-        else:
-            groups = X[self.pk].drop_duplicates().values
-            column_names = []
-            gathered_features = []
-            for group in groups:
-                current_X = X[pd.DataFrame(X[self.pk] == group).all(axis=1)]
-                fix_vad: pd.DataFrame = current_X[self.dispersion]
-                for stat in self.stats:
-                    column_names.append(
-                        f'fix_disp_{stat}_{"_".join([str(g) for g in group])}'
-                    )
-                    gathered_features.append(fix_vad.apply(stat))
-
-        features_df = pd.DataFrame(data=[gathered_features], columns=column_names)
-
         return features_df if self.return_df else features_df.values
