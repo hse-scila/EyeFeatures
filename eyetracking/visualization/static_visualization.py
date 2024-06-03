3565e41042fa0a05b062d3c66652570c9cb98a3c
ZaharKondaurov <stallmens@yandex.ru>
2024-06-03 21:46:48 +0300

    Add hull visualization and other visualization improvments

--- eyetracking/visualization/static_visualization.py
+++ eyetracking/visualization/static_visualization.py
@@ -1,8 +1,11 @@
-from typing import List
+from typing import Dict
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
+
+from scipy.spatial import ConvexHull
 
 
 def _cmap_generation(n: int):
@@ -29,14 +32,21 @@ def scanpath_visualization(
     is_vectors: bool = False,
     min_dispersion: float = 1.2,
     max_velocity: float = 4.7,
-    aoi_c: List[str] = None,
+    aoi_c: Dict[str, mpl.colors] = None,
     only_points: bool = False,
+    seq_colormap: bool = False,
+    show_hull: bool = False,
+    show_legend: bool = False,
+    path_to_img: str = None,
 ):
     plt.figure(figsize=fig_size)
     eps = 1e-8
+    marks = ("o", "^", "s", "*", "p")
+    legend = []
     data = data_.copy()
     if aoi is not None:
         data.dropna(subset=[aoi], inplace=True)
+
     data.reset_index(inplace=True, drop=True)
     X, Y = data[x], data[y]
     dX, dY = data[x].diff(), data[y].diff()
@@ -45,9 +55,12 @@ def scanpath_visualization(
     reg_only = XY[(XY.iloc[:, 0] < 0) | (XY.iloc[:, 1] > 0)]
 
     if duration is not None:
-        dur = data[duration]
-        dur /= dur.max()
-        fixation_size = np.array(dur * points_width)
+        dur_intervals = np.linspace(0, data[duration].max(), 6)
+        for i in range(1, len(dur_intervals)):
+            legend.append(
+                f"[{round(dur_intervals[i - 1], 2)}, {round(dur_intervals[i], 2)})"
+            )
+
     if dispersion is not None:
         disp = data[dispersion]
         disp /= disp.max()
@@ -57,15 +70,90 @@ def scanpath_visualization(
         plt.imshow(plt.imread(img_path))
 
     if aoi is None:
-        plt.scatter(X, Y, s=fixation_size, color=points_color)
+        if duration is None:
+            plt.scatter(X, Y, s=fixation_size, color=points_color)
+        else:
+            for i in range(1, len(dur_intervals)):
+                points_dur = data[
+                    (data[duration] >= dur_intervals[i - 1])
+                    & (data[duration] < dur_intervals[i])
+                ]
+                plt.scatter(
+                    x=points_dur[x],
+                    y=points_dur[y],
+                    label=legend[i - 1],
+                    edgecolors="black",
+                    marker=marks[i - 1],
+                )
     else:
-        ind = 0
+        once = True
         if aoi_c is None:
-            aoi_c = plt.cm.get_cmap(lut=len(data[aoi].drop_duplicates().values))
+            get_aoi_cm = plt.cm.get_cmap(lut=len(data[aoi].drop_duplicates().values))
+            aoi_c = dict()
+            for i in range(len(data[aoi].unique())):
+                aoi_c[data[aoi].unique()[i]] = get_aoi_cm(i)
         for area in data[aoi].drop_duplicates().values:
             points = data[data[aoi] == area]
-            plt.scatter(x=points[x], y=points[y], color=aoi_c(ind), label=area)
-            ind += 1
+            if duration is not None:
+                for i in range(1, len(dur_intervals)):
+                    points_dur = points[
+                        (points[duration] >= dur_intervals[i - 1])
+                        & (points[duration] < dur_intervals[i])
+                    ]
+                    if once:
+                        plt.scatter(
+                            x=points_dur[x],
+                            y=points_dur[y],
+                            color=aoi_c[area],
+                            label=legend[i - 1],
+                            edgecolors="black",
+                            marker=marks[i - 1],
+                        )
+                    else:
+                        plt.scatter(
+                            x=points_dur[x],
+                            y=points_dur[y],
+                            color=aoi_c[area],
+                            edgecolors="black",
+                            marker=marks[i - 1],
+                        )
+            else:
+                if not show_hull:
+                    plt.scatter(
+                        x=points[x],
+                        y=points[y],
+                        color=aoi_c[area],
+                        label=area,
+                        edgecolors="black",
+                    )
+                else:
+                    plt.scatter(
+                        x=points[x],
+                        y=points[y],
+                        color=aoi_c[area],
+                        edgecolors="black",
+                    )
+            if show_hull:
+                points_num = np.array([points[x].to_numpy(), points[y].to_numpy()])
+                points_num = points_num.T
+
+                hull = ConvexHull(points_num)
+                for simplex in hull.simplices:
+                    plt.plot(
+                        points_num[simplex, 0],
+                        points_num[simplex, 1],
+                        "-",
+                        color=aoi_c[area],
+                    )
+                plt.fill(
+                    points_num[hull.vertices, 0],
+                    points_num[hull.vertices, 1],
+                    color=aoi_c[area],
+                    alpha=0.5,
+                    label=area,
+                )
+            once = False
+    if show_legend:
         plt.legend()
 
     if points_enumeration:
@@ -77,48 +165,59 @@ def scanpath_visualization(
         plt.show()
         return
 
+    c_sac = np.array(mpl.colors.to_rgb(path_color))
+    if seq_colormap:
+        ls = np.linspace(0, 1, len(fixation_size))
+    else:
+        ls = np.ones(len(fixation_size))
+
     if not is_vectors:
-        plt.plot(
-            [X.iloc[:-1], X.shift(-1).iloc[:-1]],
-            [Y.iloc[:-1], Y.shift(-1).iloc[:-1]],
-            color=path_color,
-            linewidth=path_width,
-        )
+        for i in range(len(X) - 1):
+            plt.plot(
+                [X.iloc[i], X.iloc[i + 1]],
+                [Y.iloc[i], Y.iloc[i + 1]],
+                color=c_sac * ls[i],
+                linewidth=path_width,
+            )
         if regression_color is not None:
+            c_reg = np.array(mpl.colors.to_rgb(regression_color))
             regX = [(X.iloc[i - 1], X.iloc[i]) for i in reg_only.index]
             regY = [(Y.iloc[i - 1], Y.iloc[i]) for i in reg_only.index]
             for i in range(len(regX)):
-                plt.plot(regX[i], regY[i], color=regression_color, linewidth=path_width)
+                plt.plot(regX[i], regY[i], color=c_reg * ls[i], linewidth=path_width)
     else:
-        plt.quiver(
-            X.iloc[:-1],
-            Y.iloc[:-1],
-            X.shift(-1).iloc[:-1] - X.iloc[:-1],
-            Y.shift(-1).iloc[:-1] - Y.iloc[:-1],
-            angles="xy",
-            scale_units="xy",
-            color=path_color,
-            scale=1,
-            width=path_width / 500,
-            edgecolor="yellow",
-            linewidth=path_width / 1000,
-        )
-        if regression_color is not None:
-            regX = np.array([(X.iloc[i - 1], X.iloc[i]) for i in reg_only.index]).T
-            regY = np.array([(Y.iloc[i - 1], Y.iloc[i]) for i in reg_only.index]).T
+        for i in range(len(X) - 1):
             plt.quiver(
-                regX[0],
-                regY[0],
-                regX[1] - regX[0],
-                regY[1] - regY[0],
+                X.iloc[i],
+                Y.iloc[i],
+                X.iloc[i + 1] - X.iloc[i],
+                Y.iloc[i + 1] - Y.iloc[i],
                 angles="xy",
                 scale_units="xy",
-                color=regression_color,
+                color=c_sac * ls[i],
                 scale=1,
                 width=path_width / 500,
                 edgecolor="yellow",
                 linewidth=path_width / 1000,
             )
+        if regression_color is not None:
+            c_reg = np.array(mpl.colors.to_rgb(regression_color))
+            regX = np.array([(X.iloc[i - 1], X.iloc[i]) for i in reg_only.index])
+            regY = np.array([(Y.iloc[i - 1], Y.iloc[i]) for i in reg_only.index])
+            for i in range(len(regX)):
+                plt.quiver(
+                    regX[i][0],
+                    regY[i][0],
+                    regX[i][1] - regX[i][0],
+                    regY[i][1] - regY[i][0],
+                    angles="xy",
+                    scale_units="xy",
+                    color=c_reg * ls[i],
+                    scale=1,
+                    width=path_width / 500,
+                    edgecolor="yellow",
+                    linewidth=path_width / 1000,
+                )
 
     if micro_sac_color is not None:  # TODO (doesn't work with normalized coordinates)
         assert (
@@ -141,5 +240,9 @@ def scanpath_visualization(
             plt.plot(
                 mic_sacX[i], mic_sacY[i], color=micro_sac_color, linewidth=path_width
             )
-    plt.show()
+    if path_to_img is not None:
+        plt.axis("off")
+        plt.savefig(path_to_img)
+        plt.axis("on")
+    # plt.show()
     return
