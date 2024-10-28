626f50c283c157eeff99ed9a20f7c88f6acf8207
Vagiz Daudov <vagiz.0282@gmail.com>
2024-10-28 11:53:32 +0300

    Adding Spinx config file

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Test Project'
+author = 'The Authors'
+version = release = '0.1.0'
+
+# -- General configuration ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = []
+extensions = []
+language = 'en'
+master_doc = 'index'
+pygments_style = 'sphinx'
+# source_suffix = '.rst'
+# templates_path = ['_templates']
+
+# -- Options for HTML output ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
