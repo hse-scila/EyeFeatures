37b1d96b35a853f8c457cf2af94bc7432cebb37f
Vagiz Daudov <vagiz.0282@gmail.com>
2024-10-28 16:58:20 +0300

    Change config

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,18 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'EyeFeatures'
+copyright = '2024'
 author = ''
 version = release = '0.1.0'
 
 # -- General configuration ------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-exclude_patterns = []
 extensions = [
     'm2r2',
     'sphinx.ext.autodoc',
@@ -32,6 +32,7 @@ master_doc = 'index'
 pygments_style = 'sphinx'
 source_suffix = ['.rst', '.md']
 # templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
